d12c8d1f957fba2caeeeedb860967a2d5e6c69b3
vivek997 <vivekrawal997@gmail.com>
2018-03-23 12:17:12 +0530

    my first commit in main fille

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to spy chat")
+spy_name = input("What is your name?")
+spy_salutation = input("What should we call you (Mr. or Ms.)?")
+spy_name = spy_salutation + " " + spy_name
+print("Alright "+spy_name+" I'd like to know a little bit more about you...")
+spy_age = int(input("What's your age?"))
+print("You are "+str(spy_age)+" years old.")
